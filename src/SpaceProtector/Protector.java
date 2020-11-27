214bac1be60bca05ffdef783e42e5e544b36fa9e
MangoChili <374776276@qq.com>
2020-11-26 23:18:18 -0600

    Update superclass Sprite and class Protector
    Initialization of protector, the icon, and movement.

--- src/SpaceProtector/Protector.java
+++ src/SpaceProtector/Protector.java
@@ -1,5 +1,61 @@
 package SpaceProtector;
 
-public class Protector extends Sprite {
+import javax.swing.*;
+import java.awt.event.*;
 
+public class Protector extends Sprite {
+	
+	int INIT_X_COORD = 270;
+	int INIT_Y_COORD = 280;
+	int BOARD_WIDTH = 360;
+	
+	private int width;
+	int step;
+	
+	public Protector() {
+		initialProtector();
+	}
+	
+	private void initialProtector() {
+		var protectorSprite = "res/shuttle2.png";
+		var icon = new ImageIcon(protectorSprite);
+		width = icon.getImage().getWidth(null);
+		setImage(icon.getImage());
+		setX(INIT_X_COORD);
+		setY(INIT_Y_COORD);
+	}
+	
+	public void move() {
+        x += step;
+        
+        // prevent our sprite out of edge
+        if (x <= 2) {
+            x = 2;
+        }
+        if (x >= BOARD_WIDTH - 2 * width) {
+            x = BOARD_WIDTH - 2 * width;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_LEFT) {
+        	step = -2;
+        }
+        if (key == KeyEvent.VK_RIGHT) {
+        	step = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_LEFT) {
+        	step = 0;
+        }
+        if (key == KeyEvent.VK_RIGHT) {
+        	step = 0;
+        }
+    }
 }
