0911dd6c7585730179132587e388ce6d37664f08
MangoChili <374776276@qq.com>
2020-12-02 03:12:25 -0600

    Invaders bug fixed
    Invaders bug fixed, no longer move out of right edge.

---
+++ Protector.java
@@ -0,0 +1,61 @@
+package SpaceProtector;
+
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+public class Protector extends Sprite {
+	
+	int INIT_X_COORD = 140;
+	int INIT_Y_COORD = 360;
+	int BOARD_WIDTH = 360;
+	
+	private int width;
+	int step;
+	
+	public Protector() {
+		initialProtector();
+	}
+	
+	private void initialProtector() {
+		var protectorSprite = "src/SpaceProtector/res/shuttle2.png";
+		var icon = new ImageIcon(protectorSprite);
+		width = icon.getImage().getWidth(null);
+		setImage(icon.getImage());
+		setX(INIT_X_COORD);
+		setY(INIT_Y_COORD);
+	}
+	
+	public void move() {
+        x += step;
+        
+        // prevent our sprite out of edge
+        if (x <= 2) {
+            x = 2;
+        }
+        if (x >= BOARD_WIDTH - width - 18) {
+            x = BOARD_WIDTH - width - 18;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_A) {
+        	step = -2;
+        }
+        if (key == KeyEvent.VK_D) {
+        	step = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_A) {
+        	step = 0;
+        }
+        if (key == KeyEvent.VK_D) {
+        	step = 0;
+        }
+    }
+}
